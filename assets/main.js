bf99321824555fd490ca33c6638c2f9bb3a8bbdb
matheusrajeduzz <matheus.mendes@eduzz.com>
2019-10-31 20:14:33 -0300

    ta feio mas ta bom

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+    const items = [
+        {nome: 'Laranjais', descricao: 'Casa amarela grande', proprietario: 'aldair', type: 1},
+        {nome: 'Laranjais', descricao: 'Casa amarela grande', proprietario: 'aldair', type: 2},
+        {nome: 'Laranjais', descricao: 'Casa amarela grande', proprietario: 'aldair', type: 1},
+        {nome: 'Laranjais', descricao: 'Casa amarela grande', proprietario: 'aldair', type: 3},
+        {nome: 'Laranjais', descricao: 'Casa amarela grande', proprietario: 'aldair', type: 1}
+    ];
+
+    const item = {nome: 'Laranjais', descricao: 'Casa amarela grande', proprietario: 'aldair', type: 1};
+}
+
+consoleSomething = (text) => {
+    console.log(text);
+}
